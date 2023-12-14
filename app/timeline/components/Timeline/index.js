d1aba24a6bc934fc7f20299d6c34425ce1215b6a
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-12-14 19:17:51 +0100

    Seperate year's total words from overall entries total words

--- app/timeline/components/Timeline/index.js
+++ app/timeline/components/Timeline/index.js
@@ -7,24 +7,27 @@ import { Tooltip } from "react-tooltip";
 import formatEntries from "./lib/formatEntries";
 
 export default function Timeline({ entries }) {
-    const { value, totalWords } = formatEntries(entries);
-    const [year, setYear] = useState(Object.keys(value)[0]);
+    const { data, totalWords } = formatEntries(entries);
+    const [year, setYear] = useState(Object.keys(data)[0]);
     const [selected, setSelected] = useState("");
+
+    const value = data[year].entries;
+    const totalYearWords = data[year].totalWords;
 
     return (
         <>
             <select value={year} onChange={(e) => setYear(e.target.value)}>
-                {Object.keys(value).map((year) => (
+                {Object.keys(data).map((year) => (
                     <option key={year} value={year}>
                         {year}
                     </option>
                 ))}
             </select>
             <p>
-                {totalWords} {totalWords < 2 ? "word" : "words"} in {year}
+                {totalYearWords} {totalYearWords == 1 ? "word" : "words"} in {year} / {totalWords} total
             </p>
             <HeatMap
-                value={value[year]}
+                value={value}
                 weekLabels={["", "Mon", "", "Wed", "", "Fri", ""]}
                 monthLabels={["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
                 startDate={new Date(`${year}/01/01`)}
@@ -50,7 +53,7 @@ export default function Timeline({ entries }) {
                             className="cell-tooltip"
                             data-tooltip-content={
                                 data.count
-                                    ? `${data.count} ${data.count < 2 ? "word" : "words"} on ${data.date}`
+                                    ? `${data.count} ${data.count == 1 ? "word" : "words"} on ${data.date}`
                                     : `No entry on ${data.date}`
                             }
                             {...props}
@@ -60,14 +63,14 @@ export default function Timeline({ entries }) {
             />
             <Tooltip anchorSelect=".cell-tooltip" place="bottom" />
             {selected !== "" &&
-                (selected.count > 0 ? (
+                (selected.id ? (
                     <>
                         <p>
-                            {selected.count} {selected.count < 2 ? "word" : "words"} on{" "}
+                            {selected.count} {selected.count == 1 ? "word" : "words"} on{" "}
                             <Link href={`/entries/${selected.id}`}>{selected.date}</Link>
                         </p>
                         <ul>
-                            {selected.bullets.map((bullet) => (
+                            {selected.bullets?.map((bullet) => (
                                 <li key={bullet.id}>{bullet.text}</li>
                             ))}
                         </ul>
--- app/timeline/components/Timeline/lib/formatEntries.js
+++ app/timeline/components/Timeline/lib/formatEntries.js
@@ -1,26 +1,34 @@
 export default function formatEntries(entries) {
-    const value = {};
+    const data = {};
     let totalWords = 0;
 
     entries.forEach((entry) => {
         const year = entry.date.split("-")[0];
-        if (!value[year]) value[year] = [];
-        const bullets = entry.bullets.map((bullet) => ({
+        if (!data[year]) {
+            data[year] = {
+                entries: [],
+                totalWords: 0,
+            };
+        }
+
+        const bullets = entry.bullets?.map((bullet) => ({
             id: bullet.id,
             text: bullet.text,
         }));
 
-        const words = bullets.reduce((total, bullet) => total + bullet.text.split(" ").length, 0);
+        const words = bullets?.reduce((total, bullet) => total + bullet.text.split(" ").length, 0) || 0;
 
-        value[year].push({
+        data[year].entries.push({
             id: entry.id,
             date: entry.date,
             count: words,
             bullets: bullets,
         });
 
+        data[year].totalWords += words;
+
         totalWords += words;
     });
 
-    return { value, totalWords };
+    return { data, totalWords };
 }
