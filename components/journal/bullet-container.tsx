17f9e746b940f6157d89ec9a8508ceb35c3a014a
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-04-13 17:15:24 +0200

    Add whitespace-pre-line styling to bullet text

--- components/journal/bullet-container.tsx
+++ components/journal/bullet-container.tsx
@@ -4,7 +4,7 @@ export default function BulletContainer({ bullet }: { bullet: Bullet }) {
     return (
         <div className="flex gap-2">
             <div className="shrink-0 w-2 h-2 rounded-full bg-blue-500 mt-2"></div>
-            <p className="word-break">{bullet.text}</p>
+            <p className="word-break whitespace-pre-line">{bullet.text}</p>
         </div>
     );
 }
